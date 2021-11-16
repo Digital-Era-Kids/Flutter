0096d5ccf3e69061443c4d2f7e9e5abdb3e50e2d
Karan <karan.gaikwad512@gmail.com>
2021-11-16 23:15:27 +0530

    Login: move to background

--- lib/view/pages/signin/login.dart
+++ lib/view/pages/signin/login.dart
@@ -3,6 +3,7 @@ import 'package:digital_era_kids/view/pages/signin/forgotPassword.dart';
 import 'package:flutter/material.dart';
 import 'package:email_validator/email_validator.dart';
 import 'package:digital_era_kids/services/auth_services.dart';
+import 'package:move_to_background/move_to_background.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -24,170 +25,176 @@ class _LoginState extends State<Login> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Container(
-              color: Color(0xff2329D6),
-              padding: EdgeInsets.all(40),
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height * 0.3,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  CircleAvatar(
-                    radius: 50,
-                    backgroundColor: Colors.white,
-                    child: ClipOval(
-                      child: new SizedBox(
-                          height: MediaQuery.of(context).size.height * 0.8,
-                          width: MediaQuery.of(context).size.width * 1,
-                          child: Image.asset("assets/images/icon.jpeg")),
+    return WillPopScope(
+      onWillPop: () async {
+        MoveToBackground.moveTaskToBack();
+        return false;
+      },
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                color: Color(0xff2329D6),
+                padding: EdgeInsets.all(40),
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height * 0.3,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    CircleAvatar(
+                      radius: 50,
+                      backgroundColor: Colors.white,
+                      child: ClipOval(
+                        child: new SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.8,
+                            width: MediaQuery.of(context).size.width * 1,
+                            child: Image.asset("assets/images/icon.jpeg")),
+                      ),
                     ),
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width * 0.5,
-                    child: Text(
-                      "Digital Era Kids",
-                      style: TextStyle(
-                          fontFamily: 'Roboto',
-                          fontSize: 36,
-                          fontWeight: FontWeight.normal,
-                          color: Colors.white),
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 30, right: 30, left: 30),
-              child: Form(
-                key: _formKey,
-                child: SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(height: 10.0),
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 15, top: 15),
-                        child: TextFormField(
-                            autovalidateMode:
-                                AutovalidateMode.onUserInteraction,
-                            validator: (value) {
-                              if (value.trim().isEmpty) {
-                                return "Please enter email id";
-                              } else if (EmailValidator.validate(value) ==
-                                  false) {
-                                return "Enter correct email id";
-                              } else {
-                                return null;
-                              }
-                            },
-                            keyboardType: TextInputType.emailAddress,
-                            controller: email,
-                            decoration: InputDecoration(
-                              prefixIcon: Icon(
-                                Icons.email_outlined,
-                                color: Color(0xff2329D6),
-                              ),
-                              enabled: true,
-                              hintText: "Enter email id",
-                              labelText: "Email",
-                              labelStyle: TextStyle(
-                                  color: Color(0xFFB8B6B6), fontSize: 12),
-                              hintStyle: TextStyle(color: Color(0xFF737373)),
-                              focusedBorder: authTfBorderOutline(true),
-                              border: authTfBorderOutline(false),
-                              enabledBorder: authTfBorderOutline(false),
-                            )),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.5,
+                      child: Text(
+                        "Digital Era Kids",
+                        style: TextStyle(
+                            fontFamily: 'Roboto',
+                            fontSize: 36,
+                            fontWeight: FontWeight.normal,
+                            color: Colors.white),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 15),
-                        child: TextFormField(
-                            obscureText: !_secure,
-                            autovalidateMode:
-                                AutovalidateMode.onUserInteraction,
-                            validator: (value) {
-                              if (value.trim().isEmpty) {
-                                return "Please enter password";
-                              } else if (value.trim().length < 8) {
-                                return 'Must contain 8-30 characters';
-                              } else {
-                                return null;
-                              }
-                            },
-                            keyboardType: TextInputType.text,
-                            controller: password,
-                            decoration: InputDecoration(
-                              prefixIcon: Icon(
-                                Icons.lock,
-                                color: Color(0xff2329D6),
-                              ),
-                              enabled: true,
-                              hintText: "Enter password",
-                              labelText: "Password",
-                              labelStyle: TextStyle(
-                                  color: Color(0xFFB8B6B6), fontSize: 12),
-                              hintStyle: TextStyle(color: Color(0xFF737373)),
-                              suffixIcon: IconButton(
-                                icon: Icon(
-                                  _secure
-                                      ? Icons.visibility
-                                      : Icons.visibility_off,
-                                  color: Color(0xFFB8B6B6),
-                                ),
-                                onPressed: () {
-                                  setState(() {
-                                    _secure = !_secure;
-                                  });
-                                },
-                              ),
-                              focusedBorder: authTfBorderOutline(true),
-                              border: authTfBorderOutline(false),
-                              enabledBorder: authTfBorderOutline(false),
-                            )),
-                      ),
-                      Align(
-                          alignment: Alignment.centerRight,
-                          child: TextButton(
-                            child: Text("Forgot password?"),
-                            onPressed: () {
-                              Navigator.of(context).push(MaterialPageRoute(
-                                  builder: (context) => ForgotPassword()));
-                            },
-                          )),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Align(
-                          alignment: Alignment.center,
-                          child: Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                color: Color(0xFFA9C938),
-                              ),
-                              padding: EdgeInsets.all(2),
-                              width: MediaQuery.of(context).size.width * 0.8,
-                              child: TextButton(
-                                child: Text("Login",
-                                    style: TextStyle(
-                                        fontFamily: 'Roboto',
-                                        color: Colors.white,
-                                        fontSize: 16)),
-                                onPressed: () {
-                                  if(_formKey.currentState.validate()){
-                                    authentication.LogIn(email.text, password.text, context);
-                                  }
-                                },
-                              ))),
-                    ],
-                  ),
+                    )
+                  ],
                 ),
               ),
-            )
-          ],
+              Container(
+                margin: EdgeInsets.only(top: 30, right: 30, left: 30),
+                child: Form(
+                  key: _formKey,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 10.0),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 15, top: 15),
+                          child: TextFormField(
+                              autovalidateMode:
+                                  AutovalidateMode.onUserInteraction,
+                              validator: (value) {
+                                if (value.trim().isEmpty) {
+                                  return "Please enter email id";
+                                } else if (EmailValidator.validate(value) ==
+                                    false) {
+                                  return "Enter correct email id";
+                                } else {
+                                  return null;
+                                }
+                              },
+                              keyboardType: TextInputType.emailAddress,
+                              controller: email,
+                              decoration: InputDecoration(
+                                prefixIcon: Icon(
+                                  Icons.email_outlined,
+                                  color: Color(0xff2329D6),
+                                ),
+                                enabled: true,
+                                hintText: "Enter email id",
+                                labelText: "Email",
+                                labelStyle: TextStyle(
+                                    color: Color(0xFFB8B6B6), fontSize: 12),
+                                hintStyle: TextStyle(color: Color(0xFF737373)),
+                                focusedBorder: authTfBorderOutline(true),
+                                border: authTfBorderOutline(false),
+                                enabledBorder: authTfBorderOutline(false),
+                              )),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 15),
+                          child: TextFormField(
+                              obscureText: !_secure,
+                              autovalidateMode:
+                                  AutovalidateMode.onUserInteraction,
+                              validator: (value) {
+                                if (value.trim().isEmpty) {
+                                  return "Please enter password";
+                                } else if (value.trim().length < 8) {
+                                  return 'Must contain 8-30 characters';
+                                } else {
+                                  return null;
+                                }
+                              },
+                              keyboardType: TextInputType.text,
+                              controller: password,
+                              decoration: InputDecoration(
+                                prefixIcon: Icon(
+                                  Icons.lock,
+                                  color: Color(0xff2329D6),
+                                ),
+                                enabled: true,
+                                hintText: "Enter password",
+                                labelText: "Password",
+                                labelStyle: TextStyle(
+                                    color: Color(0xFFB8B6B6), fontSize: 12),
+                                hintStyle: TextStyle(color: Color(0xFF737373)),
+                                suffixIcon: IconButton(
+                                  icon: Icon(
+                                    _secure
+                                        ? Icons.visibility
+                                        : Icons.visibility_off,
+                                    color: Color(0xFFB8B6B6),
+                                  ),
+                                  onPressed: () {
+                                    setState(() {
+                                      _secure = !_secure;
+                                    });
+                                  },
+                                ),
+                                focusedBorder: authTfBorderOutline(true),
+                                border: authTfBorderOutline(false),
+                                enabledBorder: authTfBorderOutline(false),
+                              )),
+                        ),
+                        Align(
+                            alignment: Alignment.centerRight,
+                            child: TextButton(
+                              child: Text("Forgot password?"),
+                              onPressed: () {
+                                Navigator.of(context).push(MaterialPageRoute(
+                                    builder: (context) => ForgotPassword()));
+                              },
+                            )),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Align(
+                            alignment: Alignment.center,
+                            child: Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: Color(0xFFA9C938),
+                                ),
+                                padding: EdgeInsets.all(2),
+                                width: MediaQuery.of(context).size.width * 0.8,
+                                child: TextButton(
+                                  child: Text("Login",
+                                      style: TextStyle(
+                                          fontFamily: 'Roboto',
+                                          color: Colors.white,
+                                          fontSize: 16)),
+                                  onPressed: () {
+                                    if(_formKey.currentState.validate()){
+                                      authentication.LogIn(email.text, password.text, context);
+                                    }
+                                  },
+                                ))),
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
